c0c88fcc960ec476f015d4d2db8af3ba7151587a
ambik <ambik@DESKTOP-RHJGD7K>
2026-04-13 11:23:55 +0530

    second branch

--- src/test/java/demo/Practices.java
+++ src/test/java/demo/Practices.java
@@ -4,8 +4,9 @@ public class Practices {
 
 	public static void main(String[] args) {
 	System.out.println("some changes in github");
-	System.out.println("Changes to github");
+//	System.out.println("Changes to github");
 	System.out.println("In Brancha");
+	System.out.println("Changes in branch");
 
 	}
 
