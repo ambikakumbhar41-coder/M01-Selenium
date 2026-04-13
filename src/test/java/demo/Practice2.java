fa2c48a0b3e0f98a4acec35dd1fb06fc63fa4947
ambik <ambik@DESKTOP-RHJGD7K>
2026-04-13 10:31:57 +0530

    Chnages is there

--- src/test/java/demo/Practice2.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class Practice2 {
-	public static void main(String[] args) {
-		System.out.println("changes to github");
-		
-	}
-
-}
--- src/test/java/demo/Practices.java
+++ src/test/java/demo/Practices.java
@@ -4,6 +4,7 @@ public class Practices {
 
 	public static void main(String[] args) {
 	System.out.println("some cahnges in github");
+	System.out.println("Changes to  github ");
 
 	}
 
