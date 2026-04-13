97e2bbf2406b132579aefc69a927faf75566223a
ambik <ambik@DESKTOP-RHJGD7K>
2026-04-13 12:12:18 +0530

    FromeATE

--- src/test/java/demo/Practices.java
+++ src/test/java/demo/Practices.java
@@ -4,9 +4,9 @@ public class Practices {
 
 	public static void main(String[] args) {
 	System.out.println("some changes in github");
-  //  System.out.println("Changes to github");
+    System.out.println("Changes to github");
 	System.out.println("In Brancha");
-	System.out.println("Frome ATE");
+	System.out.println("Frome change ATE");
 
 
 	}
